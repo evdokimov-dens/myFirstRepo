87a5f6422e5f5e9692be0c28d1a5162518ba4d51
evdokimov-dens <evdokimov.denis96@yandex.ru>
2024-09-09 00:53:08 +0500

    произвел рассчеты в файле js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "myFirstProject";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 560;
+let rollback = 58;
+let fullPrice = 5000;
+let adaptive = true;
 
-alert("Hello, world!");
-console.log("Hello, world!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов: ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта: ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split());
+console.log("Процент отката посреднику за работу: " + rollback + "% (" + fullPrice * (rollback / 100) + " рублей)");
